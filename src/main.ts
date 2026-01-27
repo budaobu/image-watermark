fb6785224bd4d1b2aefebaff91414eefda33838a
bufang <1931297+budaobu@users.noreply.github.com>
2026-01-27 15:51:16 +0800

    SSG

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
-import { createApp } from 'vue'
+import { ViteSSG } from 'vite-ssg'
 import { addCollection } from '@iconify/vue'
 import { icons as heroIcons } from '@iconify-json/heroicons'
 import { icons as simpleIcons } from '@iconify-json/simple-icons'
 import './styles.css'
 import App from './App.vue'
+import Home from './pages/Home.vue'
 
-// Register icons globally for offline support
+// Register icons globally
 addCollection(heroIcons)
 addCollection(simpleIcons)
 
-createApp(App).mount('#app')
+const routes = [
+  { path: '/', component: Home }
+]
+
+export const createApp = ViteSSG(
+  App,
+  { routes },
+)
